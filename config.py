a6bd6d076919470fa098d670da5135f3d03dfd7a
Melika <meli.maghsoudi@gmail.com>
2026-05-01 15:47:38 -0700

    Config

---
+++ config.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TMDB_KEY = os.getenv(TMDB_KEY)
+
+WEIGHTS = {
+    "genre": 3,
+    "director": 4,
+    "cast": 1,
+    "keyword": 2,
+    "decade": 1,
+    "rating": 2,
+}
